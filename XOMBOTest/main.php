1d8a5001457a786c9918606e63b6c4dbcd3408e1
ColeHorvat <56736430+ColeHorvat@users.noreply.github.com>
2020-06-11 18:07:46 -0700

    Add files via upload
    Implemented second api to print user data along with the images printed originally

--- XOMBOTest/main.php
+++ XOMBOTest/main.php
@@ -2,33 +2,60 @@
 <html>
 <head>
     <title>XOMBO Skills Assessment</title>
+
+    <style>
+        img
+        {
+            vertical-align: middle;
+            padding-right: 50px;
+        }
+    </style>
 </head>
 <body>
 <h1>XOMBO Skills Assessment</h1>
 
 <?php
+    //Initialize url variables
+    $imageUrl = 'http://jsonplaceholder.typicode.com/photos';
+    $userUrl = 'http://jsonplaceholder.typicode.com/users';
 
-    $resourceUrl = 'http://jsonplaceholder.typicode.com/photos';
-    $ch = curl_init();//Initialize cURL
-    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);//Set cURL to return the result of the HTTP request
-    curl_setopt($ch, CURLOPT_URL, $resourceUrl);//Set the url for the HTTP request
-    $output = curl_exec($ch);//Execute request
-    curl_close($ch);//Close cURL request
+    //Get images from API
+    $chIm = curl_init(); //Initialize curl for images
+    curl_setopt($chIm, CURLOPT_RETURNTRANSFER, true);//Set image curl to return the result of the HTTP request
+    curl_setopt($chIm, CURLOPT_URL, $imageUrl);//Set the url for the HTTP request
+    $outputImg = curl_exec($chIm);//Execute image request
+    curl_close($chIm);//Close Image curl
+
+    //Get user data from API
+    $chUs = curl_init(); //Initialize curl for user data
+    curl_setopt($chUs, CURLOPT_RETURNTRANSFER, true);//Set user curl to return the result of the HTTP request
+    curl_setopt($chUs, CURLOPT_URL, $userUrl);//Set the url for the HTTP request
+    $outputUs = curl_exec($chUs);//Execute image request
+    curl_close($chUs);//Close Image curl
+
+    //Decode JSON data into PHP array
+    $imageArray = json_decode($outputImg, true);
+    $userArray = json_decode($outputUs, true);
 
 
-    $imageArray = json_decode($output, true);//Decode JSON result in PHP array
-
-    //echo $imageArray[0]["url"];
-
-
-
-    for($i = 0; $i < count($imageArray); $i++)
+    for($i = 0; $i < count($userArray); $i++)
     {
-        echo '<img src="'.$imageArray[$i]["url"].'"alt=""/>';//Print images from image array
+        echo '<img src="'.$imageArray[$i]["url"].'"alt=""/>';//Print image
+
+
+        echo '<div class="userInfo" style="padding-left: 50px; display: inline-block">';//Create text div
+
+        //Print user info
+        echo "Name: ".$userArray[$i]["name"];//Name of user
+        echo "<br>User Id: ".$userArray[$i]["id"];//Id of user
+        echo "<br>Username: ".$userArray[$i]["username"];//Username of user
+        echo "<br>Email: ".$userArray[$i]["email"];//Email of user
+        echo "<br>Address: ".$userArray[$i]["address"]["suite"]." ".$userArray[$i]["address"]["street"].", ".$userArray[$i]["address"]["city"]." ".$userArray[$i]["address"]["zipcode"];//Full address of user
+
+        echo '</div>';//end div
+        echo "<br>";//new line
+
     }
-
-
-
 ?>
 
 </body>
